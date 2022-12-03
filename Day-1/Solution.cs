c02231e09bb6f986883d6e89bbf4705ac3272402
Harry-Ross <hazross@hotmail.com>
2022-12-03 16:11:26 +1100

    abstracted day 1

--- Day-1/Solution.cs
+++ Day-1/Solution.cs
@@ -6,26 +6,21 @@ class Day1CalorieCounter
     public static string file_name = "../../../Day-1/Input.txt";
     public static int Part1()
     {
-        string[] input_lines = File.ReadAllLines(file_name);
-        List<int> calorie_count = new List<int>();
-        int current = 0;
-        foreach (string line in input_lines)
-        {
-            if (string.IsNullOrEmpty(line))
-            {
-                calorie_count.Add(current);
-                current = 0;
-                continue;
-            }
-
-            current += int.Parse(line);
-            
-        }
+        List<int> calorie_count = GetCalorieCount();
 
         return calorie_count.Max();
     }
 
     public static int Part2()
+    {
+        List<int> calorie_count = GetCalorieCount();
+
+        calorie_count.Sort();
+        calorie_count.Reverse();
+        return calorie_count.Take(3).Sum();
+    }
+
+    private static List<int> GetCalorieCount()
     {
         string[] input_lines = File.ReadAllLines(file_name);
         List<int> calorie_count = new List<int>();
@@ -43,8 +38,6 @@ class Day1CalorieCounter
 
         }
 
-        calorie_count.Sort();
-        calorie_count.Reverse();
-        return calorie_count.Take(3).Sum();
+        return calorie_count;
     }
 }
