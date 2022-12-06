5e076edb88af9ac94414b7f8b4b0b42ccff5f24a
Harry Ross <hazross@hotmail.com>
2022-12-06 19:15:12 +1100

    day 5 part 2

--- Day-5/SupplyStacks.cs
+++ Day-5/SupplyStacks.cs
@@ -40,6 +40,54 @@
         return return_str;
     }
 
+    public static string Part2()
+    {
+        string[] input_lines = File.ReadAllLines(file_name);
+
+        int height = GetHeight(input_lines);
+
+        List<Stack<char>> stacks = GetStacks(input_lines, height);
+
+        int selection = 0;
+        int origin = 0;
+        int destination = 0;
+        string[] words = new string[6];
+
+        char current;
+
+        for (int i = height + 2; i < input_lines.Length; i++)
+        {
+            words = input_lines[i].Split(' ');
+            selection = Int32.Parse(words[1]);
+            origin = Int32.Parse(words[3]) - 1;
+            destination = Int32.Parse(words[5]) - 1;
+
+            char[] crates = new char[selection];
+
+            for (int j = 0; j < selection; j++)
+            {
+                crates[j] = stacks[origin].Pop();
+            }
+
+            Array.Reverse(crates);
+
+            foreach (char crate in crates)
+            {
+                stacks[destination].Push(crate);
+            }
+            
+        }
+
+        string return_str = "";
+
+        foreach (Stack<char> stack in stacks)
+        {
+            return_str = return_str += stack.Peek();
+        }
+
+        return return_str;
+    }
+
     private static List<Stack<char>> GetStacks(string[] input, int height)
     {
         List<Stack<char>> stacks = new List<Stack<char>>();
@@ -70,10 +118,5 @@
         }
 
         return height;
-    }
-
-    public static void Main()
-    {
-        Console.WriteLine(Part1());
     }
 }
